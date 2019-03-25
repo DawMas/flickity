c2af0c4a862d15df28d28c407f0d72874ef8e11e
Dawid Maslankiewicz <maslankiewiczd@gmail.com>
2019-03-25 20:12:05 +0100

    Finished all tasks

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/*var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  hash: true,
+  pageDots: false,
+});*/
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity('.main-carousel', {
+  hash: true,
+  cellAlign: 'left',
+  contain: true,
+  pageDots: false,
+  fullscreen: true,
+  reloadCells: true,
+  // options
+});
+
+
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
+
+
+
+
+// vanilla JS
